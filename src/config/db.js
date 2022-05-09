1fc9dcf6c65039b65ba3137b0ba5f544048fcb35
Harsh Vardhan <harsh.vardhan320@gmail.com>
2022-05-09 11:19:55 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/newdbagain
 
 const connectDB = async () => {
     try {
-        mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
             console.log('DB is connected');
         });
     }
